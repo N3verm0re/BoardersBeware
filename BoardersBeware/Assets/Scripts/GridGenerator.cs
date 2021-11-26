40dd4641d04554906e20f0274e64a4481d66a78e
Bruno Gomes <bruno.gomes.771@outlook.com>
2021-11-26 20:51:48 +0000

    Grid, Camera Follow, Movement clean up

---
+++ BoardersBeware/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid grid1 = new Grid(100, 100, 0.5f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- BoardersBeware/Assets/Scripts/MapGrid.cs
+++ BoardersBeware/Assets/Scripts/MapGrid.cs
@@ -7,10 +7,10 @@ public class Grid
     private int width;
     private int height;
     private float gridHeight;
-    private (int, int) cellSize;
+    private float cellSize;
     private int[,] grid;
 
-    public Grid(int width, int height, float gridHeight, (int, int) cellSize)
+    public Grid(int width, int height, float gridHeight, float cellSize)
     {
         this.width = width;
         this.height = height;
@@ -18,5 +18,23 @@ public class Grid
         this.cellSize = cellSize;
 
         grid = new int[width, height];
+
+        for(int x = 0; x < grid.GetLength(0); x++)
+        {
+            for(int y = 0; y < grid.GetLength(1); y++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, gridHeight, y), GetWorldPosition(x, gridHeight, y + 1), Color.blue, 100f);
+                Debug.DrawLine(GetWorldPosition(x, gridHeight, y), GetWorldPosition(x + 1, gridHeight, y), Color.blue, 100f);
+            }
+        }
+    }
+
+    public Vector3 GetWorldPosition(int x, float y, float z)
+    {
+        Vector3 returnVector = new Vector3(x, y);
+        returnVector *= cellSize;
+        returnVector.z = z;
+
+        return returnVector;
     }
 }
